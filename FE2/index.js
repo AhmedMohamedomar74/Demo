6b02fc25264a5456bc4371e88121ea757062f412
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-10-28 15:15:31 +0300

    make auth middelwae

--- FE2/index.js
+++ FE2/index.js
@@ -1,11 +1,19 @@
-const clientIO = new io("http://localhost:3000")
+const token = localStorage.getItem("token")
+
+const clientIO = new io("http://localhost:3000" , {
+    auth : 
+    {
+        acessToken : token
+    }
+})
+
 
 clientIO.emit("say-hellow" ,  {FE : "Success"})
 
-clientIO.on("say-hellow" , (data) =>
-{
-    console.log(data)
-})
+// clientIO.on("say-hellow" , (data) =>
+// {
+//     console.log(data)
+// })
 
 clientIO.on("say-back" , (data)=>
 {
--- src/Gateways/soketio.gateway.js
+++ src/Gateways/soketio.gateway.js
@@ -1,6 +1,30 @@
 import { Server } from "socket.io"
+import { verify } from "../utils/secuirty/token.services.js"
 
 let ioServer = null
+const connectedSokets = new Map()
+
+const authmiddelware = async (Socket, next) => {
+    // console.log()
+    const clientToken = Socket.handshake.auth.acessToken
+    const client = await verify({ token: clientToken, key: process.env.USER_ACESS_TOKEN_SIGNATURE })
+    Socket.data = {
+        userID: client._id
+    }
+
+
+    if (connectedSokets.has(client._id)) {
+        // If user exists, add the new socket ID to their array
+        const existingSockets = connectedSokets.get(client._id)
+        connectedSokets.set(client._id, [...existingSockets, Socket.id])
+    } else {
+        // If user doesn't exist, create a new array with the socket ID
+        connectedSokets.set(client._id, [Socket.id])
+    }
+
+    console.log({ inside_middelware: true, client })
+    next()
+}
 
 export const intializer = (httpServer) => {
     ioServer = new Server(httpServer, {
@@ -8,11 +32,19 @@ export const intializer = (httpServer) => {
             origin: "*", // Configure this properly for production
         }
     })
-
+    ioServer.use(authmiddelware)
     ioServer.on("connection", (socket) => {
         console.log(socket.id)
 
+        socket.on("say-hellow", (data) => {
+            console.log(data)
+
+            console.log(socket.data)
+        })
+        console.log(connectedSokets)
     })
+
+
 }
 
 export const getIo = () => {
